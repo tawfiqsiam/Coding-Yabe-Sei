22621614198090320cfe0127aa9d6d1546807018
tawfiqsiam <toto.siam999@gmail.com>
2018-12-13 13:41:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -255,15 +255,6 @@ hours = 12;
   }
 });
 
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-client.on('ready', () => console.log('Yo this ready!'));
-
-client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
-
-client.on('reconnecting', () => console.log('I am reconnecting now!'));
 
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
