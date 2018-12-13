4ae2d4409dd757bf935ca12dcde92fce2d7a52fd
tawfiqsiam <toto.siam999@gmail.com>
2018-12-13 07:38:38 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
+const randomPuppy = require('random-puppy');
 const fs = require("fs");
 
 const client = new Discord.Client();
