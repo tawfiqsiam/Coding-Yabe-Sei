c74781776d4715e4728d160a181ce809a0f8e200
tawfiqsiam <toto.siam999@gmail.com>
2018-12-13 20:01:35 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     let embed = new Discord.RichEmbed()
 
     .setColor(client.config.embedColor)
-    .setDescription(`[Click here to invite me to your server :3](https://discordapp.com/oauth2/authorize?client_id=506254054339379220&scope=bot&permissions=8)`)
+    .setDescription(`[Click here to invite me to your server :3](https://discord.gg/FmPM6ha)`)
 
     message.channel.send(embed)
 }
