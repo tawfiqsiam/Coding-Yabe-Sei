cf027ff9ef494f9badd7e892293a3107d606b79f
tawfiqsiam <toto.siam999@gmail.com>
2018-12-13 11:25:56 +0200

    Delete up.js

--- commands/up.js
+++
@@ -1,7 +0,0 @@
-const moment = require("moment")
-require("moment-duration-format")
-
-exports.run = (client, message, args) => {
-    const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-    message.channel.send(duration)
-}
