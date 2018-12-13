b93f0b6d487aafbfc626bc5ed117c3ddf20379ba
tawfiqsiam <toto.siam999@gmail.com>
2018-12-13 11:43:29 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,54 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
 exports.run = (client, message, args) => {
-    message.channel.send("```NSFW Commands:\n 4k\n amateur\n asian\n ass\n boobs\n cosplay\n gif\n hentai\n fuck\n pornhub\n pussy\n snapchat\n milf\n bbw\n dick\n public\n uniform\n\nOther Commands: \n help\n invite```")
+    if (args[0]) {
+        fs.readdir("./commands/", (err, files) => {
+            if (err) console.error(err);
+
+            try {
+                files.forEach(file => {
+                    let commands = [];
+                    let props = require(`./${file}`);
+                    let cmdName = props.help.name;
+                    let cmdDescription = props.help.description;
+                    let cmdUsage = props.help.usage;
+                    commands.push(cmdName);
+
+                    if (commands.includes(args[0])) {
+                        if (args[0] == file.split('.')[0]) {
+                            const embed = new Discord.RichEmbed()
+                                .addField(`**${cmdName[0].toUpperCase() + cmdName.slice(1)} Command**`, cmdDescription)
+                                .addField('Usage', cmdUsage)
+                                .setColor(client.config.embedColor)
+
+                                message.channel.send(embed);
+                            }
+                    }
+                });
+            } catch (err) {
+                message.channel.send("An error has occured while processing your request. if this persists send a bug report using ` hope`");
+            }
+        });
+    } else {
+        const embed = new Discord.RichEmbed()
+            .setColor(client.config.embedColor)
+            .addField("**Changelog**", "You can check the latest changes with `skrrt changelog`")
+            .addField("**Commands**", "List of all available commands\nFor additional help with a command, type `skrrt help <command name>`\nThank you for using the Yabe beta bot! We hope you will stick with us!")
+            .addField("**Moderation** *`each require certain perms`*", "`say` `kick` `ban` `rem`")
+            .addField("**Fun**", "`meme` `poke` `slap`")
+            .addField("**Games**", "`games are coming very soon!`")
+            .addField("**Learning**", "`just dont try tp learn codes just buy bots from me")
+            .addField("**Random**", "`roles` `roll` `addrole` `ping` `help` `serverinfo` `info` `bug` `invite` `whois` `avatar` `changelog` `translate` `uptime` `osu`")
+            .setFooter("Created by Jacob")
+            .setTimestamp()
+    
+        message.channel.send(embed)
+    }
+}
+
+exports.help = {
+    name: "help",
+    description: "The `help` command displays a help menu, which contains a list of commands and a way to find further help.",
+    usage: "`skrrt help`"
 }
