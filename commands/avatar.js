a6190d2c61aa7922f3ad469635f738e7ff79184e
tawfiqsiam <toto.siam999@gmail.com>
2018-12-29 14:08:43 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -29,5 +29,5 @@ exports.run = (client, message, args) => {
 exports.help = {
 	name: "avatar",
 	description: "The `avatar` command sends the specified user's profile avatar on Discord. Leave empty to get your own avatar.",
-	usage: "`yabe avatar <desired user(without @)/leave blank for own>`",
+	usage: "`!avatar <desired user(without @)/leave blank for own>`",
 }
